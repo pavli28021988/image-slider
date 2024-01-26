3166838650ad4a7da786725dae98a1d53613204d
Pavlina Ivanova <pavlina33331988@gmail.com>
2024-01-26 18:35:43 +0200

    added
    Image-slider

---
+++ app.js
@@ -0,0 +1,44 @@
+﻿(function(){
+
+    const pictures = [
+        "img1",
+        "img2",
+        "img3",
+        "img4",
+        "img5",
+        "img6"
+    ]
+
+    const buttons = document.querySelectorAll('.btn');
+    const imgContainer = document.querySelector('.img-container');
+    let counter=0;
+
+    buttons.forEach((button)=>{
+        button.addEventListener('click', ()=>{
+
+            if(button.classList.contains('btn-left')){
+                counter--;
+                if(counter < 0){
+                    counter = pictures.length-1;
+                }
+
+                imgContainer.style.background = `url('img/${pictures[counter]}.avif')center center/cover  no-repeat`
+            }
+
+            if(button.classList.contains('btn-right')){
+                counter++;
+                if(counter > pictures.length-1){
+                   counter = 0;
+                }
+
+                imgContainer.style.background = `url('img/${pictures[counter]}.avif') center center/cover  no-repeat`
+            }
+
+
+        })
+    })
+
+
+
+
+})();
